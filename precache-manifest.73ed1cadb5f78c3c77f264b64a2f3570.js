d65fa88142dc0d7f9869f327a7eb278a3fa20263
Cameron Avelis <cmavelis@gmail.com>
2020-08-24 14:08:38 -0400

    Updates

---
+++ precache-manifest.73ed1cadb5f78c3c77f264b64a2f3570.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50e2b1770164b52fb4408e453e875331",
+    "url": "/board-demo/index.html"
+  },
+  {
+    "revision": "3e3605c8e71ca08e5ae3",
+    "url": "/board-demo/static/css/main.fe147e04.chunk.css"
+  },
+  {
+    "revision": "d1fd57363c624c0e4f8c",
+    "url": "/board-demo/static/js/2.209ec572.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/board-demo/static/js/2.209ec572.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e3605c8e71ca08e5ae3",
+    "url": "/board-demo/static/js/main.fc7e4b1b.chunk.js"
+  },
+  {
+    "revision": "04eba7f79657e75dac63",
+    "url": "/board-demo/static/js/runtime-main.1088a66b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/board-demo/static/media/logo.5d5d9eef.svg"
+  }
+]);
